1d1fd5e4ba215d23c49e6b29976dfc78af1164be
Harsha <sriharshasepana424@gmail.com>
2026-07-04 15:40:13 +0530

    Implement FeedForward network with dropout

---
+++ model/transformer/feed_forward.py
@@ -0,0 +1,43 @@
+import torch
+import torch.nn as nn
+
+from utils.config import ModelConfig
+
+
+class FeedForward(nn.Module):
+    """
+    Position-wise Feed Forward Network.
+
+    Architecture:
+
+        Linear
+            ↓
+        GELU
+            ↓
+        Dropout
+            ↓
+        Linear
+    """
+
+    def __init__(self, config: ModelConfig):
+        super().__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(
+                config.embedding_dim,
+                config.feed_forward_dim
+            ),
+            nn.GELU(),
+            nn.Dropout(config.dropout),
+            nn.Linear(
+                config.feed_forward_dim,
+                config.embedding_dim
+            )
+        )
+
+    def forward(
+        self,
+        x: torch.Tensor
+    ) -> torch.Tensor:
+
+        return self.network(x)
